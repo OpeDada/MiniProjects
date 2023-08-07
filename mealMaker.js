51cf2e02daa37f040ec7cce768737963fe22c471
OpeDada <opedada@gmail.com>
2023-08-07 22:12:34 +0900

    make use of getters and setters to generate meal and price for a restaurant

---
+++ mealMaker.js
@@ -0,0 +1,29 @@
+const menu = {
+  _meal: "",
+  _price: 0,
+
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return (this._meal = mealToCheck);
+    }
+  },
+
+  set price(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (this._price = priceToCheck);
+    }
+  },
+
+  get todaysSpecial() {
+    return this._meal && this._price
+      ? `Today’s Special is ${this._meal} for $${this._price}!`
+      : "Meal or price was not set correctly!";
+  },
+};
+
+menu._meal = "pizza";
+menu._price = 9;
+
+// console.log(menu)
+
+console.log(menu.todaysSpecial);
