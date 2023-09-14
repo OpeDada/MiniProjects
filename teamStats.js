e53cc3ee148c7e060b5216141944a6d78513ad77
OpeDada <opedada@gmail.com>
2023-09-14 23:22:43 +0900

    create data with array and object data structures

---
+++ teamStats.js
@@ -0,0 +1,41 @@
+const team = {
+  _players: [
+    { firstName: "John", lastName: "Lucas", age: 45 },
+    { firstName: "Luke", lastName: "Jobs", age: 50 },
+    { firstName: "Mark", lastName: "Musk", age: 55 },
+  ],
+  _games: [
+    { opponent: "Gunners", teamPoints: 13, opponentPoints: 16 },
+    { opponent: "lakers", teamPoints: 23, opponentPoints: 14 },
+    { opponent: "Moon", teamPoints: 20, opponentPoints: 12 },
+  ],
+
+  get players() {
+    return this._players;
+  },
+
+  get games() {
+    return this._games;
+  },
+
+  addPlayer(newFirstName, newLastName, newAge) {
+    let player = {
+      firstName: newFirstName,
+      lastName: newLastName,
+      age: newAge,
+    };
+    this._players.push(player);
+  },
+
+  addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+    let game = {
+      opponent: newOpponent,
+      teamPoints: newTeamPoints,
+      opponentPoints: newOpponentPoints,
+    };
+    this._games.push(game);
+  },
+};
+
+team.addGame("Titans", 100, 98);
+console.log(team.games);
